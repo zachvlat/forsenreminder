4403ec0d844d7aa1d9c3720341a6cd21db530eeb
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-01-18 16:35:44 +0200

    Add files via upload

---
+++ forsenReminder.py
@@ -0,0 +1,17 @@
+import requests
+
+myStreamer = input("type 'forsen' to see if forsen is live: ")
+
+url = "https://gwyo-twitch.p.rapidapi.com/stream/" + myStreamer
+
+headers = {
+	"X-RapidAPI-Key": "ff9402c761mshc899600518864e1p1cdb2fjsn523d5efaa070",
+	"X-RapidAPI-Host": "gwyo-twitch.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+if response.text == "{}" :
+    print ("forsen is ded feelsbadman")
+else:
+    print("LIVE KKool playing guitar")
